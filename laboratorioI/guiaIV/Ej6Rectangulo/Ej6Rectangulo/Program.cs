c4d824686c78b4f66d36144f76e4b5396bef0454
Patricio Albornoz <wagnerpatricio@outlook.com.ar>
2023-04-21 12:26:44 -0300

    agregado ej6 en csharp

---
+++ laboratorioI/guiaIV/Ej6Rectangulo/Ej6Rectangulo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej6Rectangulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double baseRectangulo, alturaRectangulo;
+
+			Console.WriteLine("Ingrese la base de la figura: ");
+			baseRectangulo = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Ingrese la altura de la figura: ");
+            alturaRectangulo = Convert.ToDouble(Console.ReadLine());
+
+			if (baseRectangulo == alturaRectangulo)
+			{
+				Console.WriteLine("Es un cuadrado");
+			} else if (baseRectangulo > alturaRectangulo)
+			{
+                Console.WriteLine("Es un rectangulo horizontal");
+            } else
+			{
+                Console.WriteLine("Es un rectangulo vertical");
+            }
+
+            Console.WriteLine("El area del rectangulo es: {0}", baseRectangulo*alturaRectangulo);
+            Console.ReadKey();
+        }
+    }
+}
