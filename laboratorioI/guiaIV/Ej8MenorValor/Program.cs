eeb08fb45d5f2d95c803d25fcf2d4ea0cc543039
Patricio Albornoz <wagnerpatricio@outlook.com.ar>
2023-04-23 17:49:40 -0300

    ej8 agregado

--- laboratorioI/guiaIV/Ej8MenorValor/Program.cs
+++ laboratorioI/guiaIV/Ej8MenorValor/Program.cs
@@ -1,15 +1,56 @@
-ï»¿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+// Este codigo ha sido generado por el modulo psexport 20180802-w32 de PSeInt.
+// Es posible que el codigo generado no sea completamente correcto. Si encuentra
+// errores por favor reportelos en el foro (http://pseint.sourceforge.net).
 
-namespace Ej8MenorValor
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-        }
-    }
+using System;
+
+namespace PSeInt {
+	class Ej8MenorValor {
+
+		static void Main(string[] args) {
+
+			int contador,menor,num,orden;
+			contador = 0;
+			orden = 1;
+
+			Console.WriteLine("Ingrese el primer valor: ");
+			num = Convert.ToInt32(Console.ReadLine());
+			menor = num;
+			contador = contador+1;
+
+			Console.WriteLine("Ingrese el segundo valor: ");
+			num = Convert.ToInt32(Console.ReadLine());
+			if (num<menor) {
+				menor = num;
+				orden = contador;
+			}
+
+			Console.WriteLine("Ingrese el tercer valor: ");
+			num = Convert.ToInt32(Console.ReadLine());
+			if (num<menor) {
+				menor = num;
+				orden = contador;
+			}
+
+			Console.WriteLine("Ingrese el cuarto valor: ");
+			num = Convert.ToInt32(Console.ReadLine());
+			if (num<menor) {
+				menor = num;
+				orden = contador;
+			}
+
+			Console.WriteLine("Ingrese el quinto valor: ");
+			num = Convert.ToInt32(Console.ReadLine());
+			if (num<menor) {
+				menor = num;
+				orden = contador;
+			}
+
+			Console.WriteLine("El menor valor es: {0}, fue ingresado en: {1}° lugar.", menor, orden);
+			Console.ReadKey();
+		}
+
+	}
+
 }
+
