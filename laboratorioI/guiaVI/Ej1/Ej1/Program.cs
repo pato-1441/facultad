89768b4521468bc1d80342e544befc24a0e35d12
Patricio Albornoz <wagnerpatricio@outlook.com.ar>
2023-05-04 22:52:41 -0300

    ej1 guiaVI

---
+++ laboratorioI/guiaVI/Ej1/Ej1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Ej1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cantViajes, edad = 0, valorTicket = 3, totalViajes = 0, totalDia = 0;
+
+            Console.BackgroundColor = ConsoleColor.DarkRed;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WindowHeight = 25;
+            Console.WindowWidth = 75;
+            Console.Clear();
+            Console.WriteLine("Ingrese la cantidad de viajes a realizar:");
+            cantViajes = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= cantViajes; i++)
+            {
+                Console.WriteLine("Inicio del viaje n°: {0}", i);
+                Console.WriteLine("Ingrese la edad: ");
+                edad = Convert.ToInt32(Console.ReadLine());
+
+                do
+                {
+                    Console.WriteLine("Valor ticket: ${0}", valorTicket);
+                    totalViajes++;
+                    Console.WriteLine("Ingrese la edad del siguiente: (-1: para finalizar viaje)");
+                    edad = Convert.ToInt32(Console.ReadLine());
+                } while (edad != -1);
+                
+
+                Console.WriteLine("Recaudacion viaje n°{0}: ${1}",i, totalViajes*valorTicket);
+                totalDia = (totalViajes * valorTicket) + totalDia;
+                totalViajes = 0;
+            }
+            Console.WriteLine("Recaudacion del día: ${0}", totalDia);           
+            Console.WriteLine("Presione una tecla para salir");
+            Console.ReadKey();
+        }
+    }
+}
